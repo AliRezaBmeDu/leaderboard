c6ae39b7005ceb45f9e0f5fbee2f1eb4e7335add
AliReza <ali.reza.buet@gmail.com>
2023-08-02 23:17:14 +0600

    Add eventlisteners, working perfectly fine

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import './style.css';
-import scorecard from './modules/scorelist';
+
+var scorecard = [];
 
 const baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api';
-let gameId = null;
+let gameId = null || localStorage.getItem('gameID');
 
+//Function for rendering the scoreboard
+const renderLeaderboard= async() => {
+    const scoreboardElement = document.getElementById('scoreboard');
+    scoreboardElement.innerHTML = '';
+    scorecard.sort((a, b) => b.score - a.score);
+    console.log('Is it calling the scorecard?', scorecard);
+    for (let i = 0; i < scorecard.length; i += 1) {
+      const listItem = document.createElement('li');
+      listItem.textContent = `${scorecard[i].user} - ${scorecard[i].score}`;
+  
+      // Add appropriate IDs and classes to the list items
+      listItem.setAttribute('id', `player-${i + 1}`);
+      listItem.classList.add('player-item');
+  
+      scoreboardElement.appendChild(listItem);
+    }
+  }
+  
 //Function for creating a new Game
-const createNewGame = async () => {
+const createNewGame = async() => {
     try{
         const response = await fetch(`${baseUrl}/games/`, {
             method: 'POST',
@@ -14,11 +33,12 @@ const createNewGame = async () => {
             },
             body: JSON.stringify({ name: 'My cool game' })
         });
-
+        console.log('data', response);
         const data = await response.json();
         gameId = data.result;
 
         console.log(`Game created with ID: ${gameId}`);
+        localStorage.setItem('gameID', gameId);
     }
     catch (error) {
         console.error('Error creating a new game:', error);
@@ -28,13 +48,15 @@ const createNewGame = async () => {
 //Function to get all the scores of the created game
 const getScores = async () => {
     try{
-        if(!gameId){
-            console.error('The Game is not created yet');
-            return
-        }
         const response =  await fetch(`${baseUrl}/games/${gameId}/scores/`);
         const data = await response.json();
-        scorecard.push(data.result);
+
+        console.log('getScoresData',data)
+
+        scorecard = await data.result;
+        console.log('scorecard:',scorecard);
+
+        renderLeaderboard();
     }
     catch (error) {
         console.error('Error fetching scores:', error)
@@ -44,11 +66,7 @@ const getScores = async () => {
 // Function to save a new score for the game created by you
 const saveScore = async (user, score) => {
     try {
-      if (!gameId) {
-        console.error('No game is created yet.');
-        return;
-      }
-  
+    
       const response = await fetch(`${baseUrl}/games/${gameId}/scores/`, {
         method: 'POST',
         headers: {
@@ -64,29 +82,33 @@ const saveScore = async (user, score) => {
     }
   };
 
-
-//Function for rendering the scoreboard
-function renderLeaderboard() {
-  const scoreboardElement = document.getElementById('scoreboard');
-  scoreboardElement.innerHTML = '';
-
-  for (let i = 0; i < scorecard.length; i += 1) {
-    const listItem = document.createElement('li');
-    listItem.textContent = `${scorecard[i].name} - ${scorecard[i].score}`;
-
-    // Add appropriate IDs and classes to the list items
-    listItem.setAttribute('id', `player-${i + 1}`);
-    listItem.classList.add('player-item');
-
-    scoreboardElement.appendChild(listItem);
-  }
-}
-
-// Call the function to create a new game when the page loads
 if(!gameId){
-    createNewGame();
+    createNewGame()
 }
 
+const handleSubmitButtonClick = () => {
+    const nameInput = document.getElementById('nameInput');
+    const scoreInput = document.getElementById('scoreInput');
+    const user = nameInput.value.trim();
+    const score = Number(scoreInput.value);
+  
+    saveScore(user, score);
+  
+    // Clear the input fields after saving the score
+    nameInput.value = '';
+    scoreInput.value = '';
+  };
 
-// Call the function to initially render the leaderboard
-renderLeaderboard();
+// Function to handle the "Refresh" button click
+const handleRefreshButtonClick = () => {
+    getScores();
+};
+
+// Add event listeners to buttons
+document.getElementById('refreshBtn').addEventListener('click', handleRefreshButtonClick);
+document.getElementById('submitBtn').addEventListener('click', handleSubmitButtonClick);
+
+console.log('Scorecard condition:', scorecard);
+
+
+
---
+++ src/modules/functions.js
@@ -0,0 +1,3 @@
+const baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api';
+export let gameId = null || localStorage.getItem('gameID');
+
