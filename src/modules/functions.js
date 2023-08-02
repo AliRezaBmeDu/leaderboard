be392ea24787f499f2b9acbacd26b3152724ba9c
AliReza <ali.reza.buet@gmail.com>
2023-08-02 23:45:54 +0600

    Fix the linters

--- src/modules/functions.js
+++ src/modules/functions.js
@@ -7,7 +7,6 @@ const renderLeaderboard = async () => {
   const scoreboardElement = document.getElementById('scoreboard');
   scoreboardElement.innerHTML = '';
   scorecard.sort((a, b) => b.score - a.score);
-  console.log('Is it calling the scorecard?', scorecard);
   for (let i = 0; i < scorecard.length; i += 1) {
     const listItem = document.createElement('li');
     listItem.textContent = `${scorecard[i].user} - ${scorecard[i].score}`;
@@ -30,7 +29,6 @@ export const createNewGame = async () => {
       },
       body: JSON.stringify({ name: 'My cool game' }),
     });
-    console.log('data', response);
     const data = await response.json();
     gameId = data.result;
 
@@ -46,11 +44,7 @@ export const getScores = async () => {
   try {
     const response = await fetch(`${baseUrl}/games/${gameId}/scores/`);
     const data = await response.json();
-
-    console.log('getScoresData', data);
-
     scorecard = await data.result;
-    console.log('scorecard:', scorecard);
 
     renderLeaderboard();
   } catch (error) {
