1f87cf3ded2f1348fb3ece706e377cdec1114c81
AliReza <ali.reza.buet@gmail.com>
2023-08-02 11:11:23 +0600

    Fix stylelint errors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import './style.css';
 
 const scorecard = [
-    { name: "Ali", score: "100" },
-    { name: "Jacob", score: "97" },
-    { name: "Isac", score: "89" },
-    { name: "Freddy", score: "88" },
+  { name: 'Ali', score: '100' },
+  { name: 'Jacob', score: '97' },
+  { name: 'Isac', score: '89' },
+  { name: 'Freddy', score: '88' },
 ];
 
 function renderLeaderboard() {
@@ -14,7 +14,7 @@ function renderLeaderboard() {
   for (let i = 0; i < scorecard.length; i++) {
     const listItem = document.createElement('li');
     listItem.textContent = `${scorecard[i].name} - ${scorecard[i].score}`;
-    
+
     // Add appropriate IDs and classes to the list items
     listItem.setAttribute('id', `player-${i + 1}`);
     listItem.classList.add('player-item');
